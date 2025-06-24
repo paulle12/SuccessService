d2231df6608a75d21f580c14fc46f7bae790c71c
Paul Le <paul.le0027@gmail.com>
2025-06-24 13:18:22 -0400

    add loggers

--- src/main/java/com/sucess/SuccessService/service/SuccessService.java
+++ src/main/java/com/sucess/SuccessService/service/SuccessService.java
@@ -1,7 +1,10 @@
 package com.sucess.SuccessService.service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sucess.SuccessService.client.PartyTrackerClient;
 import com.sucess.SuccessService.client.RaiderIOHydrationClient;
@@ -18,6 +21,8 @@ public class SuccessService {
     private final RaiderIOHydrationClient hydrationClient;
     private final SuccessScoreCalculationHydrationClient scoreCalculatorClient;
 
+    private static final Logger log = LoggerFactory.getLogger(SuccessService.class);
+
     public SuccessService(
             PartyTrackerClient partyClient,
             RaiderIOHydrationClient hydrationClient,
@@ -28,27 +33,55 @@ public class SuccessService {
     }
 
     public List<HydratedSuccessPlayer> getHydratedParty(int targetLevel, String name, String realm) {
-        List<PartyMember> baseParty = partyClient.getLatestParty(name, realm);
+        log.info("Fetching latest party for name={}, realm={}, targetLevel={}", name, realm, targetLevel);
+
+        List<PartyMember> baseParty;
+        try {
+            baseParty = partyClient.getLatestParty(name, realm);
+            log.info("Successfully fetched party of size: {}", baseParty.size());
+        } catch (Exception e) {
+            log.error("Failed to fetch latest party: {}", e.getMessage(), e);
+            throw e;
+        }
 
         List<HydratedSuccessPlayer> hydratedParty = baseParty.stream().map(member -> {
-            String region = "us";
-            HydratedPlayer raiderIOHydratedPlayer = hydrationClient.hydrate(region, member.getRealm(),
-                    member.getName());
-            double playerScore = scoreCalculatorClient.getPlayerScore(raiderIOHydratedPlayer, targetLevel);
-            HydratedSuccessPlayer enrichedPlayer = new HydratedSuccessPlayer();
-            enrichedPlayer.setName(raiderIOHydratedPlayer.getName());
-            enrichedPlayer.setRealm(raiderIOHydratedPlayer.getRealm());
-            enrichedPlayer.setCharacterClass(raiderIOHydratedPlayer.getCharacterClass());
-            enrichedPlayer.setRole(raiderIOHydratedPlayer.getRole());
-            enrichedPlayer.setRecentRuns(raiderIOHydratedPlayer.getRecentRuns());
-            enrichedPlayer.setCurrentSeasonScore(raiderIOHydratedPlayer.getCurrentSeasonScore());
-            enrichedPlayer.setPreviousSeasonScore(raiderIOHydratedPlayer.getPreviousSeasonScore());
-            enrichedPlayer.setIsMainRole(raiderIOHydratedPlayer.getIsMainRole());
-            enrichedPlayer.setActiveSpecRole(raiderIOHydratedPlayer.getActiveSpecRole());
-            enrichedPlayer.setScore(playerScore);
-            return enrichedPlayer;
-        }).collect(Collectors.toList());
+            try {
+                log.info("Hydrating player: {}-{}", member.getName(), member.getRealm());
 
+                String region = "us";
+                HydratedPlayer raiderIOHydratedPlayer = hydrationClient.hydrate(region, member.getRealm(),
+                        member.getName());
+
+                log.info("Successfully hydrated: {}-{}", raiderIOHydratedPlayer.getName(),
+                        raiderIOHydratedPlayer.getRealm());
+
+                double playerScore = scoreCalculatorClient.getPlayerScore(raiderIOHydratedPlayer, targetLevel);
+
+                log.info("Calculated score {} for player: {}-{}", playerScore, raiderIOHydratedPlayer.getName(),
+                        raiderIOHydratedPlayer.getRealm());
+
+                HydratedSuccessPlayer enrichedPlayer = new HydratedSuccessPlayer();
+                enrichedPlayer.setName(raiderIOHydratedPlayer.getName());
+                enrichedPlayer.setRealm(raiderIOHydratedPlayer.getRealm());
+                enrichedPlayer.setCharacterClass(raiderIOHydratedPlayer.getCharacterClass());
+                enrichedPlayer.setRole(raiderIOHydratedPlayer.getRole());
+                enrichedPlayer.setRecentRuns(raiderIOHydratedPlayer.getRecentRuns());
+                enrichedPlayer.setCurrentSeasonScore(raiderIOHydratedPlayer.getCurrentSeasonScore());
+                enrichedPlayer.setPreviousSeasonScore(raiderIOHydratedPlayer.getPreviousSeasonScore());
+                enrichedPlayer.setIsMainRole(raiderIOHydratedPlayer.getIsMainRole());
+                enrichedPlayer.setActiveSpecRole(raiderIOHydratedPlayer.getActiveSpecRole());
+                enrichedPlayer.setScore(playerScore);
+
+                return enrichedPlayer;
+
+            } catch (Exception e) {
+                log.error("Failed to hydrate or score player {}-{}: {}", member.getName(), member.getRealm(),
+                        e.getMessage(), e);
+                return null; // Or throw new RuntimeException(e); if you prefer to bubble it up
+            }
+        }).filter(Objects::nonNull).collect(Collectors.toList());
+
+        log.info("Final hydrated party size: {}", hydratedParty.size());
         return hydratedParty;
     }
 
